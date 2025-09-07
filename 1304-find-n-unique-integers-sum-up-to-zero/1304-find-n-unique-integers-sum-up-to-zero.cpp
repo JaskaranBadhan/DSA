506b08239802b8e4d5a33082a786b21c6ad03c8f
JaskaranBadhan <150879404+JaskaranBadhan@users.noreply.github.com>
2025-09-08 00:08:36 +0530

    Update 1304-find-n-unique-integers-sum-up-to-zero.cpp

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     vector<int> sumZero(int n) {
-        vector<int> result (n);//making result array to stor 
+        vector<int> result (n);//making result array to store
+        //Approach 1
+        
         int start = 1;//start index
         int i=0;
         while(i+1<n){
@@ -10,6 +12,19 @@ public:
             i+=2;//increment 
             start++;//start increment
         }
-        return result;
+        //Approach 2 = Using two pointer 
+        
+        int i =0;//starting i from zero
+        int j = n-1;//j at last index of the array
+        int start = 1;//starting from 1
+        while(i<j){
+            result[i]= start;//first element
+            result[j]= -start;//neg of that element
+            start++;
+            i++;
+            j--;
+        }
+        //At i == j the element will out of loop and will initialie 0 automatically as vector intially assign with zero 
+        return result;//return result element
     }
 };
