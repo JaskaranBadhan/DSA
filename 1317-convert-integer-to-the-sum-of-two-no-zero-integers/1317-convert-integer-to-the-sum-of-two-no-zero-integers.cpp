e73faf5cf90c2ec7ac4c055e2114b89371eea052
JaskaranBadhan <150879404+JaskaranBadhan@users.noreply.github.com>
2025-09-08 18:07:35 +0530

    Update 1317-convert-integer-to-the-sum-of-two-no-zero-integers.cpp

--- 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -20,3 +20,24 @@ public:
         return{};//return empty vector if not possible
     }
 };
+/*Approach -2(Optimal)
+    vector<int>getNoZeroIntegers(int n){
+        int a = n;
+        int b = 0;
+        int placeValue = 1;
+        //a+b==n;
+        while(n>1){//will reduce from a and add to b till n>1
+            int take = 1;//take = 1
+            if(n %10 ==1){
+            ///if n = 2031 so reducing 1 will give 2000 so remove 2 instead of this case
+                take = 2;
+            }
+            a = a - take*placeValue;//multiply take value into placeValue
+            b = b + take*placeValue;
+            n = (n-take)/10;//moving to next digit
+            placeValue *=10;//increase placeValue like 0 10 100 etc.
+        }
+        return {a,b};//return digit 
+    }
+    //No use of check function in approach -2
+*/
